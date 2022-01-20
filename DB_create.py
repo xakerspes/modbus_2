11655fcd58cf53df79c9d42b899d5607307d1f77
xakerspes <xakerspes@gmail.com>
2022-01-20 22:46:25 +0500

    6

--- DB_create.py
+++ DB_create.py
@@ -1,3 +1,19 @@
 # """Create DB and modified"""
-import  time
+import time
 import  datetime
+
+class DataBase:
+    """create, update and insert to DB"""
+
+    def __init__(self):
+        pass
+
+    def create_table(self):
+        pass
+
+    def update(self):
+        pass
+
+    def insert(self):
+        pass
+
